42063ddb10977ee035cce898009a36aed6fcca2e
Vinesh tk <vineshtk10@gmail.com>
2024-01-30 15:01:02 +0530

    push from ubuntu

--- main.js
+++ main.js
@@ -19,8 +19,33 @@ function toNumber(string) {
     else{
         return 0;
     }
-    
+}  
 
+// 'this' functionality in the JS code
+
+function add(x, y){
+    return x*y;
 }
+
+const addTwo = add.bind(null,2);
+const addFive = add.bind(null, 3);
+
+console.log(addTwo(2))
+console.log(addFive(4))
+
+const obj = {
+    name: 'Bob',
+    getName: function(){
+        return this.name;
+    }
+}
+
+console.log(obj.name);
+console.log(obj.getName());
+const testThis = obj.getName;
+console.log(testThis());
+
+
+
 
 module.exports = toNumber;
